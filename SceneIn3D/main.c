c3e230957faafe377eb648285468524b78a630bd
Mahalia E <maev8907@colorado.edu>
2020-09-23 13:16:05 -0600

    Initial Commit

---
+++ SceneIn3D/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SceneIn3D
+//
+//  Created by Mahalia Evans on 9/23/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
